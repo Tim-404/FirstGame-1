125c1452434c3934483c89859974ac1e5d95bfbe
Brandon Xie <30637265+xiushak@users.noreply.github.com>
2020-12-22 21:11:00 -0800

    first import
    complete test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public GameObject CompleteLevelUI;
+
+    public void EndGame()
+    {
+        if (!gameHasEnded)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+    }
+
+    public void CommpleteLevel()
+    {
+        CompleteLevelUI.SetActive(true);
+    }
+
+    void Restart ()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerMovemnt.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovemnt : MonoBehaviour 
+{
+
+    public Rigidbody rb;
+    public float forwardsForce = 2000f;
+    public float sidewaysForce = 500f;
+
+    // Use FixedUpdate because we are messing with physics
+    void FixedUpdate() 
+    {
+        rb.AddForce(0, 0, forwardsForce * Time.deltaTime);
+        
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-1 * sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -1f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
